50a018c381f435d471b4234a2b93f49683fb94aa
Илья Мурадьян <leopardile@yandex.ru>
2017-03-03 16:41:25 +0300

    Добавил второе задание.

---
+++ AlgNT_2/Alg_NT_2/Alg_NT_2.cpp
@@ -0,0 +1,11 @@
+#include <mpirxx.h>
+#include <iostream>
+
+int main(){
+
+	mpz_class m1("9999999999999899879999999999999999999999999999"), m2(1);
+	mpz_class mr = m1 + m2;
+
+	std::cout << mr << std::endl;
+	return 0;
+}
