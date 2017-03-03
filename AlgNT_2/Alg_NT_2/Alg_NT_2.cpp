b792ea44a026a36213390d7db4474e849002069b
ileasile <leopardile@yandex.ru>
2017-03-03 18:51:26 +0300

    Убрал вывод "Для продолжения нажмите любую клавишу..." путём редактирования файла проекта

--- AlgNT_2/Alg_NT_2/Alg_NT_2.cpp
+++ AlgNT_2/Alg_NT_2/Alg_NT_2.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 int main(){
-
 	mpz_class m1("9999999999999899879999999999999999999999999999"), m2(1);
 	mpz_class mr = m1 + m2;
-
 	std::cout << mr << std::endl;
+
+	system("pause");
 	return 0;
 }
